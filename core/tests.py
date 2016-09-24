1ce16dc0502f3af1f01fa005d00466323ed7a096
Chris H <crhowell1@gmail.com>
2016-09-24 15:50:10 -0700

    Add more tests

--- core/tests.py
+++ core/tests.py
@@ -5,22 +5,34 @@ from .models import Mineral
 
 DATA = [
     {
-        "name": "Test Mineral 1",
+        "name": "ATest Mineral 1",
         "image_filename": "200px-Test_Mineral_1.jpg",
         "image_caption": "A Testy Mineral",
         "category": "",
         "formula": "",
         "mohs_scale_hardness": "1.0",
-        "group": "Other"
+        "group": "Oxides",
+        "color": "green"
     },
     {
-        "name": "Test Mineral 2",
+        "name": "ATest Mineral 2",
         "image_filename": "200px-Test_Mineral_2.jpg",
         "image_caption": "A Testy Mineral",
         "category": "",
         "formula": "",
         "mohs_scale_hardness": "2.0",
-        "group": "Other"
+        "group": "Carbonates",
+        "color": "red"
+    },
+    {
+        "name": "CTest Mineral 3",
+        "image_filename": "200px-Test_Mineral_2.jpg",
+        "image_caption": "A Testy Mineral",
+        "category": "",
+        "formula": "",
+        "mohs_scale_hardness": "2.0",
+        "group": "Other",
+        "color": "blue"
     }
 ]
 
@@ -36,6 +48,34 @@ class MineralTest(TestCase):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(response.context['mineral_list']), 2)
+
+    def test_mineral_by_letter(self):
+        response = self.client.get('/by-letter/C/')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.context['mineral_list']), 1)
+        self.assertContains(response, 'CTest Mineral 3')
+
+    def test_colors_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(len(response.context['color_list']), 0)
+
+    def test_colors_choice(self):
+        response = self.client.get('/by-color/red/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(len(response.context['color_list']), 0)
+        self.assertContains(response, 'ATest Mineral 2')
+
+    def test_group_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(len(response.context['group_list']), 0)
+
+    def test_group_choice(self):
+        response = self.client.get('/by-group/Oxides/')
+        self.assertEqual(response.status_code, 200)
+        self.assertNotEqual(len(response.context['group_list']), 0)
+        self.assertContains(response, 'ATest Mineral 1')
 
     def test_mineral_detail(self):
         mineral = Mineral.objects.first()
